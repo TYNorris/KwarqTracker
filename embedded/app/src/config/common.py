b67d23126023e0d0565ae05ab9b92f2fb9a3b5a3
Travis Norris <travisynorris@gmail.com>
2021-07-26 18:25:41 -0400

    Add connection to OrientDB for members

--- embedded/app/src/config/common.py
+++ embedded/app/src/config/common.py
@@ -1,8 +1,16 @@
 import logging
 import os
 
+from .secrets import db_password
+
 class Config():
     BASE_DIR = os.getcwd()
 
     LOG_LEVEL = logging.INFO
     LOG_PATH = os.path.join(BASE_DIR, "logs", "app.log")
+
+    ORIENT_DB_ADDRESS = "localhost"
+    ORIENT_DB_LOGIN = {
+        "user": "root",
+        "password": db_password
+    }
--- embedded/app/src/storage/helper.py
+++ embedded/app/src/storage/helper.py
@@ -1,8 +1,34 @@
+
+import pyorient
+
 from . import demo_users
 from app.src.user.user import User
+from app.src.config import get_config
+
+config = get_config()
 
 
 class StorageHelper:
+    client: pyorient.OrientDB
+    session_id: object
+
+    def __init__(self):
+        self.client = pyorient.OrientDB(config.ORIENT_DB_ADDRESS, 2424)
+        self.session_id = self.client.connect(config.ORIENT_DB_LOGIN["user"], config.ORIENT_DB_LOGIN["password"])
+        self.client.db_open("Members",
+                            config.ORIENT_DB_LOGIN["user"],
+                            config.ORIENT_DB_LOGIN["password"],
+                            pyorient.DB_TYPE_DOCUMENT)
 
     def get_user(self, uid: int) -> User:
-        return demo_users.get(uid)
+        response = self.client.query(
+            f"SELECT FROM Members "
+            f"WHERE uid = {uid}",
+            1
+        )
+        if not response:
+            return None
+
+        output = User(**response[0].oRecordData)
+        print(output)
+        return output
