45c393cd9fe1334fe7576230cd6dafc7846646d6
Travis Norris <travisynorris@gmail.com>
2021-07-26 21:06:55 -0400

    Add support for list type in database

--- embedded/app/src/user/user.py
+++ embedded/app/src/user/user.py
@@ -17,6 +17,9 @@ class User:
         if isinstance(self.dates_attended, str):
             self.dates_attended = self._parse_dates_from_string(self.dates_attended)
 
+        if isinstance(self.dates_attended, list):
+            self.dates_attended = self.parse_dates_from_list(self.dates_attended)
+
     def attended(self, date: datetime):
         if date.date() not in self.dates_attended:
             self.dates_attended.add(date.date())
@@ -42,7 +45,10 @@ class User:
 
     def _parse_dates_from_string(self, string) -> set:
         splits = string.split(',')
+        return self.parse_dates_from_list(splits)
+
+    def parse_dates_from_list(self, l) -> set:
         output = set()
-        for s in splits:
+        for s in l:
             output.add(date.fromisoformat(s))
         return output
